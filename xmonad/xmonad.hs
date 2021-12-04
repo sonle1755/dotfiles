414d69576654a858c1f93f91d683ee946238c1c1
sonle1755 <son.le1755@gmail.com>
2021-12-04 20:21:53 +0700

    Update

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,25 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+rofiCmd = "rofi -show drun"
+main = do
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ docks def
+        { layoutHook = avoidStruts  $  layoutHook def
+        , terminal = "alacritty"
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        , ((mod1Mask, xK_p), spawn rofiCmd)
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        ]
+
+
