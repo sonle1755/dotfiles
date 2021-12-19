50bc65b64a091eb6c60f326dbf118645501dd7a5
sonle175 <son.le1755@gmail.com>
2021-12-19 16:44:30 +0700

    UPDATE: Xmonad config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -17,9 +17,7 @@ main = do
                         }
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-        , ((mod1Mask, xK_p), spawn rofiCmd)
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((0, xK_Print), spawn "scrot")
+	, ((mod1Mask, xK_p), spawn rofiCmd)
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s ~/Pictures/Screenshots/%Y-%m-%d-%T-screenshot.png")
+        , ((0, xK_Print), spawn "scrot ~/Pictures/Screenshots/%Y-%m-%d-%T-screenshot")
         ]
-
-
