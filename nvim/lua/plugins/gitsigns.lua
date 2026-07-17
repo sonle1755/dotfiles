430ca5569eaf789d4c203ad57ebd57982cc6de0c
Son Le <son.le1755@gmail.com>
2026-07-17 12:55:14 +0700

    UPDATE: Nvim
    Code format

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -2,7 +2,7 @@ return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
   opts = {
-    signs = {
+    signs                        = {
       add          = { text = '┃' },
       change       = { text = '┃' },
       delete       = { text = '_' },
@@ -10,7 +10,7 @@ return {
       changedelete = { text = '~' },
       untracked    = { text = '┆' },
     },
-    signs_staged = {
+    signs_staged                 = {
       add          = { text = '┃' },
       change       = { text = '┃' },
       delete       = { text = '_' },
@@ -18,18 +18,18 @@ return {
       changedelete = { text = '~' },
       untracked    = { text = '┆' },
     },
-    signs_staged_enable = true,
-    signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-    numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
-    linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-    word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
-    watch_gitdir = {
+    signs_staged_enable          = true,
+    signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
+    numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
+    linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
+    word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
+    watch_gitdir                 = {
       follow_files = true
     },
-    auto_attach = true,
-    attach_to_untracked = false,
-    current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-    current_line_blame_opts = {
+    auto_attach                  = true,
+    attach_to_untracked          = false,
+    current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame_opts      = {
       virt_text = true,
       virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
       delay = 1000,
@@ -38,11 +38,11 @@ return {
       use_focus = true,
     },
     current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
-    sign_priority = 6,
-    update_debounce = 100,
-    status_formatter = nil, -- Use default
-    max_file_length = 40000, -- Disable if file is longer than this (in lines)
-    preview_config = {
+    sign_priority                = 6,
+    update_debounce              = 100,
+    status_formatter             = nil,   -- Use default
+    max_file_length              = 40000, -- Disable if file is longer than this (in lines)
+    preview_config               = {
       -- Options passed to nvim_open_win
       style = 'minimal',
       relative = 'cursor',
