7d468c0b2d526ba87ed80e72dc8fba2024621742
Son Le <son.le1755@gmail.com>
2026-07-17 16:09:53 +0700

    UPDATE: Nvim - lualine
    Remove trouble config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,20 +2,8 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
-    "folke/trouble.nvim",
   },
   config = function()
-    local trouble = require("trouble")
-
-    local symbols = trouble.statusline({
-      mode = "lsp_document_symbols",
-      groups = {},
-      title = false,
-      filter = { range = true },
-      format = "{kind_icon}{symbol.name:Normal}",
-      hl_group = "lualine_c_normal",
-    })
-
     require("lualine").setup({
       options = {
         theme = "gruvbox_dark",
@@ -23,21 +11,6 @@ return {
           statusline = { "neo-tree" },
         }
       },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = {
-          "filename",
-          {
-            symbols.get,
-            cond = symbols.has,
-          },
-        },
-        lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-
     })
   end,
 
