d689e6e3efae9da25340b8beff18059a3d257f87
Son Le <son.le1755@gmail.com>
2026-07-24 14:01:51 +0700

    UPDATE: Nvim - indent-blankline

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,6 +1,6 @@
-vim.opt.list = true  -- Show tabs and spaces diff
-vim.opt.listchars:append("eol:$") -- end line with $
-vim.g.indent_blankline_filetype_exclude = { 'dashboard' }
+vim.opt.list = true -- Show tabs and spaces diff
+vim.opt.listchars:append("eol:↴") -- end line with $
+-- vim.g.indent_blankline_filetype_exclude = { 'dashboard' }
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = { "BufReadPre", "BufNewFile" },
