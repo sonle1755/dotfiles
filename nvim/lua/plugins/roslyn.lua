be3c657294b4e9f7718105ae715741bf318222b9
Son Le <son.le1755@gmail.com>
2026-07-24 13:59:52 +0700

    UPDATE: Nvim - remove unneeded plugins
    none-ls, tiny-code-action, trouble, action-preview

--- nvim/lua/plugins/roslyn.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "seblyng/roslyn.nvim",
-  ---@module 'roslyn.config'
-  ---@type RoslynNvimConfig
-  opts = {
-    filewatching = "roslyn"
-    -- your configuration comes here; leave empty for default settings
-  },
-}
